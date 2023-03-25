ab52975474adee68a70fcb820da2b115570c8ad9
grigor-stoyanov <grigor.stoyanov@hotmail.com>
2023-03-25 19:32:36 +0200

    added functinality to pinned post to move/remove posts from top when being pinned/unpinned and added functionality to preload pinned posts(on navigation)

--- src/app/main/review/review-posts/review-posts.component.ts
+++ src/app/main/review/review-posts/review-posts.component.ts
@@ -17,27 +17,37 @@ import { Observable, Subscription } from 'rxjs';
 export class ReviewPostsComponent implements OnDestroy {
   user = this.userService.user$
   posts: IReview[] = [];
+  ordered_posts: IReview[] = [];
   error: IError | null = null;
   errorDetail: string | undefined;
   getReviewError: string | undefined;
   isLoading: boolean = false;
   pageCounter: number | null = null;
   subscription: Subscription;
-  pinnedPosts: Array<IReview> | undefined;
+  pinnedPosts: Array<IReview> = [];
 
   pinPost(post: IReview) {
+
     if (this.pinnedPosts?.some((v, _, ar) => v.id == post.id)) {
       this.pinnedPostsService.unpinReview(post)
+      const idx = this.ordered_posts.findIndex(v => v.id == post.id)
+      this.posts = this.posts.filter((v) => v.id != post.id)
+      if (idx > -1) {
+        this.posts.splice(idx, 0, post)
+      }
+
     } else {
       this.pinnedPostsService.pinReview(post)
+      this.posts = this.posts.filter((v) => v.id != post.id)
+      this.posts.unshift(post)
     }
   }
 
   public getClass(post: IReview) {
     let status = ''
-     this.pinnedPosts?.forEach((v, _) => {
+    this.pinnedPosts?.forEach((v, _) => {
       if (v.id == post.id) {
-         status = 'pinned'
+        status = 'pinned'
       }
     })
     return status
@@ -52,10 +62,15 @@ export class ReviewPostsComponent implements OnDestroy {
     this.apiService.getPosts(id, this.pageCounter)
       .subscribe({
         next: (data) => {
+          this.ordered_posts?.push(...data)
+          data = data.filter(v => !this.pinnedPosts.find(rm => (rm.id == v.id)))
           this.posts?.push(...data);
           this.error = null;
           this.isLoading = false;
           this.pageCounter = (!this.pageCounter) ? 2 : this.pageCounter += 1
+          if (data.length == 0) {
+            this.loadPostHandler()
+          }
         },
         error: (err) => {
           this.error = err.error;
@@ -71,8 +86,9 @@ export class ReviewPostsComponent implements OnDestroy {
 
   constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute,
     private userService: UserService, private pinnedPostsService: PinnedPostsService) {
-    this.loadPostHandler()
     this.subscription = this.pinnedPostsService.pinned_reviews$.subscribe(val => this.pinnedPosts = val)
+    this.posts.push(...this.pinnedPosts)
+    this.loadPostHandler()
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
