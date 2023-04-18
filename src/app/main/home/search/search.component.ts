62dd63c7695e8df9c312d5895d0837f3c8e50dbb
grigor-stoyanov <grigor.stoyanov@hotmail.com>
2023-04-18 20:15:48 +0300

    transform custom input into reactive form with a search field and dynamically created controlls(chips) which save the latest input value within the div

--- src/app/main/home/search/search.component.ts
+++ src/app/main/home/search/search.component.ts
@@ -4,66 +4,68 @@ import { getSearchValue } from '../+store/selectors';
 import { debounceTime, fromEvent, map, tap, merge } from 'rxjs';
 import { setSearchValue, typing } from '../+store/actions'
 import { Actions, ofType } from '@ngrx/effects';
-import { NgForm, NG_VALIDATORS, NG_VALUE_ACCESSOR, ControlValueAccessor, Form, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => SearchComponent), multi: true
-    }
-  ],
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements ControlValueAccessor, AfterViewInit,OnInit {
-  // @HostListener('input') callOnChange() {
-  //   this._onChange(this.search.nativeElement.textContent);
-  // }
-  _onChange: any = () => { };
-  _onTouched: any = () => { };
+export class SearchComponent implements OnInit {
   hidden = true;
   destroy = true;
   newSearchValue = ''
-  chips: string[] = []
-  form!:FormGroup;
 
+  form:FormGroup = this.fb.group({
+    // TODO fix search not upating properly
+    search:['',Validators.compose([Validators.maxLength(25)])],
+    chips:this.fb.array(
+      []
+      )
+  });
+  get chips(){
+    return this.form.controls['chips'] as FormArray;
+  }
   search$ = this.store.select(getSearchValue)
   // TODO Merge streams when loading is compleete to cancel spinner
   isTyping$ = this.action$.pipe(ofType(typing), map(() => true))
 
   @ViewChild('searchInput', { static: true }) searchInput!: ElementRef<HTMLInputElement>;
 
+  onInput(event:InputEventInit){
+    const value = this.searchInput.nativeElement.innerText.trim().split('\n').pop()
+    this.form.get('search')?.setValue(value)
+  }
+
+  //TODO update the form control to distinguish between types of parameters
   onKeyDown(event: KeyboardEvent) {
     if (event.key == 'Enter') {
       event.preventDefault()
       const value = this.searchInput.nativeElement.innerText.trim().split('\n').pop()
+
       if (value) {
-        this.chips = [...this.chips,value]
-        this._onChange(this.chips)
+        this.chips.push(this.fb.control(value))
+        // this.chips.setValue([...this.chips.value,value]);
+        // const [type,chip] = value.split(':')
+        // switch(type){
+        //   case 'Genre':
+        //     genreValue.push(chip)
+        //     break
+        //   case 'Year':
+        //     yearValue.push(chip)
+        //     break
+        //   default:
+        //     return
+        // }
       }
     }
   }
+
   removeChip(chip: string) {
-    this.chips = this.chips.filter(c => c != chip);
-    this._onChange(this.chips);
+    // Remove data from the form control
+    // this.chips = this.chips.filter(c => c != chip);
   }
-
-  writeValue(chips: string[]): void {
-    if (chips) {
-      this.chips = chips;
-    }
-
-  }
-  registerOnChange(fn: any): void {
-    this._onChange = fn;
-  }
-  registerOnTouched(fn: any): void {
-    this._onTouched = fn
-  }
-
 
   constructor(private store: Store, private action$: Actions,private fb:FormBuilder) {
 
@@ -76,16 +78,11 @@ export class SearchComponent implements ControlValueAccessor, AfterViewInit,OnIn
     return
 
   }
-  ngOnInit(): void {
-      this.form = this.fb.group({
-        chips:[[]]
-      })
-  }
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     // this.search$.subscribe(console.log)
-    // this.form.valueChanges
-    //   .subscribe(console.log)
+    this.form.valueChanges
+      .subscribe(console.log)
     // fromEvent(this.searchInput.nativeElement, 'input')
     //   .pipe(tap(e => this.store.dispatch(typing())), debounceTime(500), map(e => (e.target as HTMLInputElement).value))
     //   .subscribe((value) => this.store.dispatch(setSearchValue({ search: value })))
